b76b9108fd7637c88902725ac20bfa5632c808c9
vicarAmelia <98558073+vicarAmelia@users.noreply.github.com>
2022-03-30 10:37:49 -0400

    Started pause menu UI and code

--- Simon Says/Assets/Scripts/MainMenuUI.cs
+++ Simon Says/Assets/Scripts/MainMenuUI.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public Button startButton;
+    public Button exitButton;
+    public string gameSceneName;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+       startButton.onClick.AddListener(LoadGameScene); 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGameScene()
     {
-        
+        GameSceneManager.Instance.LoadScene(gameSceneName);
     }
 }
